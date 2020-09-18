e976e5c14dd44327e86d1873bfa216f717439526
99002566 <71494050+99002566@users.noreply.github.com>
2020-09-18 22:13:44 +0530

    Update calculator_operations.c

--- src/calculator_operations.c
+++ src/calculator_operations.c
@@ -30,3 +30,22 @@ int greater(int operand1, int operand2)
     else
         return operand2;
 }
+
+int primenum(int operand1)
+{
+    int i,m=0,flag=0;    
+      
+    m=operand1/2;    
+    for(i=2;i<=m;i++)    
+    {    
+    if(operand1%i==0)    
+    {    
+    return 0;   
+    flag=1;    
+    break;    
+    }    
+    }    
+    if(flag==0)    
+    return 1;  
+}
+     
