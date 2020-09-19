d72d0528ccbeb91da8276983bba804ac1ab54df3
99002566 <71494050+99002566@users.noreply.github.com>
2020-09-19 06:44:02 +0530

    Update calculator_operations.h

--- inc/calculator_operations.h
+++ inc/calculator_operations.h
@@ -72,5 +72,8 @@ int palindrome1(int operand2);
 int gcd(int operand1, int operand2);
 int lcm(int operand1, int operand2);
 int modulus(int operand1, int operand2);
+int kilometertometer(int operand1);
+int kilotocentimeter(int operand1);
+int kilotomillimeter(int operand1);
 
 #endif  /* #define __CALCULATOR_OPERATIONS_H__ */
